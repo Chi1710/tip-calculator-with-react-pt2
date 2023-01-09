5eedad478e575ade9413d2a3b0fff74e92483808
Chi <chiyantsestudio@gmail.com>
2023-01-08 21:08:30 -0500

    delete and modified starter files to have a clean workspace. add input for bill, range input for num of ppl and tip percentage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const[bill, setBill] = useState(0);
+  const[totalTip, setTotalTip] = useState(0);
+  const[numOfPpl, setNumOfPeople] = useState(0);
+  const[tipPercentage, setTipPercentage] = useState(0);
+  // const[splitTip, setSplitTip] = useState(0);
+  // const[splitTotal, setSplitTotal] = usestate(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tip Calculator</h1>
+
+      <label>Bill</label>
+      <span>$</span>
+      <input type="number" 
+            placeholder='0.00' 
+            min="0"
+            value={bill}
+            onChange={ e => setBill(e.target.value)}
+        />
+
+        <label>Tip</label>
+        <span>$</span>
+        <input type="number" 
+            placeholder='0' 
+            min="0"
+            value={totalTip}
+            onChange={ e => setTotalTip(e.target.value)}
+        />
+
+        <label>Tip Percentage</label>
+        <span>{tipPercentage}%</span>
+        <input type="range"
+                placeholder='0'
+                min="10"
+                max="50"
+                value={tipPercentage}
+                onChange = { e => setTipPercentage(e.target.value)}
+                />
+
+        <label>Number of People</label>
+        <span>{numOfPpl}</span>
+        <input type="range"
+              placeholder='1'
+              min="1"
+              value={numOfPpl}
+              onChange = { e => setNumOfPeople(e.target.value)}
+              />
+
     </div>
   );
 }
+
 
 export default App;
